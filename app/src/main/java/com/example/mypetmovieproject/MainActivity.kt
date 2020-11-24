af744944519d80db2d0d2e81ede256049113791a
bagasayn <bagasayn@gmail.com>
2020-11-24 17:42:29 +0300

    add ScrollView

--- app/src/main/java/com/example/mypetmovieproject/MainActivity.kt
+++ app/src/main/java/com/example/mypetmovieproject/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.mypetmovieproject
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val robert = findViewById<ImageView>(R.id.robert)
+        robert.clipToOutline = true
+
+        val evans = findViewById<ImageView>(R.id.evans)
+        evans.clipToOutline = true
+
+        val mark = findViewById<ImageView>(R.id.mark)
+        mark.clipToOutline = true
+
+        val hemsworth = findViewById<ImageView>(R.id.hemsworth)
+        hemsworth.clipToOutline = true
+
     }
 }
