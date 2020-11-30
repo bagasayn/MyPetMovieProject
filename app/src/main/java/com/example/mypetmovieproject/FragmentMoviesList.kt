7c65107d2c8f5ce55ba66f215d28d2cb68c98095
bagasayn <bagasayn@gmail.com>
2020-11-30 16:58:16 +0300

    Added FragmentMoviesList.kt to MainActivity.kt Created navigation to FragmentMoviesDetails.kt on pressed Card with Film Added backstack navigation

--- app/src/main/java/com/example/mypetmovieproject/FragmentMoviesList.kt
+++ app/src/main/java/com/example/mypetmovieproject/FragmentMoviesList.kt
@@ -1,5 +1,6 @@
 package com.example.mypetmovieproject
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,12 +8,30 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class FragmentMoviesList: Fragment() {
+    private var listener: ClickListener? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_movies_list,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.fragment_movie_details).setOnClickListener {
+            listener?.openMovieDetails()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is ClickListener) listener = context
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
     }
 
 
