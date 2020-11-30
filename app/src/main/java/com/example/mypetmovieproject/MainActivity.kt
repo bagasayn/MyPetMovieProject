adab73fa8726b83382cd83aa60cbce79826f2815
bagasayn <bagasayn@gmail.com>
2020-11-30 12:23:16 +0300

    Create fragment_movies_list.xml and develop layout as show in Movies screen

--- app/src/main/java/com/example/mypetmovieproject/MainActivity.kt
+++ app/src/main/java/com/example/mypetmovieproject/MainActivity.kt
@@ -7,19 +7,21 @@ import android.widget.ImageView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_movie_details)
-
-        val robert = findViewById<ImageView>(R.id.robert)
-        robert.clipToOutline = true
-
-        val evans = findViewById<ImageView>(R.id.evans)
-        evans.clipToOutline = true
-
-        val mark = findViewById<ImageView>(R.id.mark)
-        mark.clipToOutline = true
-
-        val hemsworth = findViewById<ImageView>(R.id.hemsworth)
-        hemsworth.clipToOutline = true
+        setContentView(R.layout.fragment_movies_list)
+        val miniLogo = findViewById<ImageView>(R.id.movie_avengers)
+        miniLogo.clipToOutline = true
+//        robert.clipToOutline = true
+//        val robert = findViewById<ImageView>(R.id.robert)
+//        robert.clipToOutline = true
+//
+//        val evans = findViewById<ImageView>(R.id.evans)
+//        evans.clipToOutline = true
+//
+//        val mark = findViewById<ImageView>(R.id.mark)
+//        mark.clipToOutline = true
+//
+//        val hemsworth = findViewById<ImageView>(R.id.hemsworth)
+//        hemsworth.clipToOutline = true
 
     }
 }
