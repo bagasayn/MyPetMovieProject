97815c57565adfe837180d04ffaf3286e907d5b7
bagasayn <bagasayn@gmail.com>
2020-11-30 16:58:16 +0300

    Added FragmentMoviesList.kt to MainActivity.kt Created navigation to FragmentMoviesDetails.kt on pressed Card with Film

---
+++ app/src/main/java/com/example/mypetmovieproject/ClickListener.kt
@@ -0,0 +1,5 @@
+package com.example.mypetmovieproject
+
+interface ClickListener {
+    fun openMovieDetails()
+}
--- app/src/main/java/com/example/mypetmovieproject/MainActivity.kt
+++ app/src/main/java/com/example/mypetmovieproject/MainActivity.kt
@@ -4,26 +4,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_movies_list)
+        setContentView(R.layout.activity_main)
 
+        changeRadius()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().apply {
+                add(
+                    R.id.fragment_movie_details, FragmentMoviesList()
+                ).commit()
+            }
+        }
+
+    }
+
+    private fun changeRadius() {
         val miniLogo = findViewById<ImageView>(R.id.movie_avengers)
         miniLogo.clipToOutline = true
 
-//        robert.clipToOutline = true
-//        val robert = findViewById<ImageView>(R.id.robert)
-//        robert.clipToOutline = true
-//
-//        val evans = findViewById<ImageView>(R.id.evans)
-//        evans.clipToOutline = true
-//
-//        val mark = findViewById<ImageView>(R.id.mark)
-//        mark.clipToOutline = true
-//
-//        val hemsworth = findViewById<ImageView>(R.id.hemsworth)
-//        hemsworth.clipToOutline = true
+        val robert = findViewById<ImageView>(R.id.robert)
+        robert.clipToOutline = true
 
+        val evans = findViewById<ImageView>(R.id.evans)
+        evans.clipToOutline = true
+
+        val mark = findViewById<ImageView>(R.id.mark)
+        mark.clipToOutline = true
+
+        val hemsworth = findViewById<ImageView>(R.id.hemsworth)
+        hemsworth.clipToOutline = true
+    }
+
+    override fun openMovieDetails() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_movie_details, FragmentMoviesDetails())
+            .addToBackStack(null)
+            .commit()
     }
 }
