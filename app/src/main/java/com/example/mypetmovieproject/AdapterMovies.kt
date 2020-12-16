3d0db15f85c41340159e3eed1ac22dc61bb6f417
bagasayn <bagasayn@gmail.com>
2020-12-16 17:27:06 +0300

    Created AdapterMovies.kt for RecyclerView to get data and assign it to ViewHolderMovies.kt Created view_holder_actor.xml Added RecyclerView in fragment_movies_list should show movies list in two columns

---
+++ app/src/main/java/com/example/mypetmovieproject/AdapterMovies.kt
@@ -0,0 +1,30 @@
+package com.example.mypetmovieproject
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class AdapterMovies(
+    context: Context,
+    private val movies: List<DataMoviesDetails>
+) : RecyclerView.Adapter<ViewHolderMovies>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderMovies {
+        return ViewHolderMovies(inflater.inflate(R.layout.view_holder_movie, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderMovies, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    override fun getItemCount(): Int {
+        return movies.size
+    }
+
+    fun getItem(position: Int): DataMoviesDetails = movies[position]
+
+}
--- app/src/main/java/com/example/mypetmovieproject/DataMoviesDetails.kt
+++ app/src/main/java/com/example/mypetmovieproject/DataMoviesDetails.kt
@@ -1,5 +1,19 @@
 package com.example.mypetmovieproject
-
+/**
+* Details:
+ * movie
+ * like
+ * age
+ * star1
+ * star2
+ * star3
+ * star4
+ * star5
+ * category
+ * reviews
+ * name movie
+ * time
+ */
 data class DataMoviesDetails(
     val movie: Int,
     val like: Boolean,
@@ -14,3 +28,64 @@ data class DataMoviesDetails(
     val nameMovie: String,
     val time: String
 )
+
+fun generateDataMoviesDetails(): List<DataMoviesDetails> {
+    return listOf(
+        DataMoviesDetails(
+            R.drawable.avengers_end_game,
+            false,
+            "13+",
+            starOne = true,
+            starTwo = true,
+            starThree = true,
+            starFour = true,
+            starFive = false,
+            categoryMovies = "Action, Adventure, Drama",
+            reviews = "125 Reviews",
+            nameMovie = "Avengers: End Game",
+            time = "137 min"
+        ),
+        DataMoviesDetails(
+            R.drawable.tenet,
+            true,
+            "16+",
+            starOne = true,
+            starTwo = true,
+            starThree = true,
+            starFour = true,
+            starFive = true,
+            categoryMovies = "Action, Sci-Fi, Thriller ",
+            reviews = "98 Reviews",
+            nameMovie = "Tenet",
+            time = "97 min"
+        ),
+        DataMoviesDetails(
+            R.drawable.black_widow,
+            false,
+            "13+",
+            starOne = true,
+            starTwo = true,
+            starThree = true,
+            starFour = true,
+            starFive = false,
+            categoryMovies = "Action, Adventure, Sci-Fi",
+            reviews = "38 Reviews",
+            nameMovie = "Black Widow",
+            time = "102 min"
+        ),
+        DataMoviesDetails(
+            R.drawable.wonder_woman,
+            false,
+            "16+",
+            starOne = true,
+            starTwo = true,
+            starThree = true,
+            starFour = true,
+            starFive = true,
+            categoryMovies = "Action, Adventure, Fantasy",
+            reviews = "74 Reviews",
+            nameMovie = "Wonder Woman 1984",
+            time = "120 min"
+        )
+    )
+}
